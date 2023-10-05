4e26d696703031d78d4a2e69ae527c7b14fa2c79
SmitHacker <102592902+SmitDasadia@users.noreply.github.com>
2023-10-05 18:54:59 +0530

    Feature: Add Footer; Cleanup code

--- layout/Footer.tsx
+++ layout/Footer.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
-const Footer = () => {
+const Footer: React.FC = () => {
   return (
-    <div className='bg-red-500'>Footer</div>
-  )
-}
+    <footer className="bg-[#030303] text-white body-font">
+      <div className="container px-5 py-8 mx-auto flex justify-center items-center">
+        <p className="text-lg font-semibold  ">
+          {" "}
+          <span className="text-gray-600">Create By </span>
+          <span className="text-blue-600 text-semiblod">Smit Dasadia</span>{" "}
+          &copy; {new Date().getFullYear()} CloneUi
+        </p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
